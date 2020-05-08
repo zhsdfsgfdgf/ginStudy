05000328d5365864dc96aa8eeec193ed24691635
zhaoheng <1798835790@qq.com>
2020-05-08 11:47:13 +0800

    viper配置集中化管理

--- common/database.go
+++ common/database.go
@@ -5,12 +5,27 @@ import (
 	model "ginStudy/models"
 
 	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
 )
 
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:zhaoheng@(47.105.83.40:3306)/ginStudy?charset=utf8&parseTime=True&loc=Local")
+	driverName := viper.GetString("datasource.driverName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+	db, err := gorm.Open(driverName, args)
 	if err != nil {
 		fmt.Println("数据库连接失败", err.Error())
 	}
--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"ginStudy/common"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	//读取配置
+	InitConfig()
 	db := common.InitDB()
 	defer db.Close()
 	r := gin.Default()
 	r = CollectRoute(r)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
+}
+
+func InitConfig() {
+	//获取当前的工作目录
+	workDir, _ := os.Getwd()
+	//设置读取的文件名,文件类型,路径
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
